1523b705bae086e6dbcfb475744e44d5bff3db48
Mengqing Qian <kaylaqian@users.noreply.github.com>
2018-02-27 15:23:28 -0800

    Update fangte.py

--- fangte.py
+++ fangte.py
@@ -9,6 +9,7 @@ ap = {}
 userTest = []
 minResult = {}
 corUser = []
+user =[]
 
 def convolution(verticesFile, edgesFile):
 	vf = open(verticesFile, 'r')
@@ -22,38 +23,45 @@ def convolution(verticesFile, edgesFile):
 	for row in csv.reader(ef, delimiter = ','):
 		if(row[-1].split(' ')[0] == '2017-08-18'):
 			time.append(row)
-	for j in range(0, 100):
+			user.append(row[0])
+    # no_dupes = [x for n, x in enumerate(a) if x not in user[:n]]
+    # print no_dupes
+	for j in range(0, 2):
 		userTest.append(time[j][0])
 		ipTime = [0 for a in range(175)]
 		for i in range(0, len(time) -1):
-			index = ap.get(time[i][1])
-			cur = (''.join(time[i])).split(' ')[-1].split(':')
-			curTime = float(cur[0])*60*60 + float(cur[1])*60 + float(cur[2])
-			ipTime[index] =  curTime
+			if(time[i][0] == time[j][0]):
+				index = ap.get(time[i][1])
+				cur = (''.join(time[i])).split(' ')[-1].split(':')
+				curTime = int(cur[0])*60*60 + int(cur[1])*60 + int(cur[2])
+				ipTime[index] =  curTime
+		print ipTime
 		users.update({time[j][0]:ipTime})
 
-	for k in range(0, 100):
+	for k in range(0, 2):
+		c=[]
 		a = time[k][0]
-		for z in range(k, 100):
+		for z in range(k+1, 2):
 			b = time[z][0]
-			curMin = np.convolve(users.get(a), users.get(b), 'same')
+			c=[users.get(a)[i] - users.get(b)[i] for i in range(len(users.get(a)))]
+			c =  np.array(c)
+			curMin = c.dot(np.transpose(c))
 			minRes = curMin
-	print minRes
-
-def buildheap(corUser):
-	heapq.heapify(corUser)
-	if len(heap) >= 5:  
-	    top_item = heap[0] # smallest in heap  
-	    if top_item < item: # min heap  
-	      top_item = heapq.heappop(heap)  
-	      print "pop", top_item,  
-	      heapq.heappush(heap, item)  
-	      print "push", item,  
-    else:
-    	heapq.heappush(heap, item)  
-    	print "push", item,  
+		print minRes
+# def buildheap(corUser):
+# 	heapq.heapify(corUser)
+# 	if len(heap) >= 5:  
+# 	    top_item = heap[0] # smallest in heap  
+# 	    if top_item < item: # min heap  
+# 	      top_item = heapq.heappop(heap)  
+# 	      print "pop", top_item,  
+# 	      heapq.heappush(heap, item)  
+# 	      print "push", item,  
+#     else:
+#     	heapq.heappush(heap, item)  
+#     	print "push", item,  
  
-	heap.sort()  
+# 	heap.sort()  
 
 def main():
 	convolution('fangte-vertices.csv', 'fangte-edges.csv')
